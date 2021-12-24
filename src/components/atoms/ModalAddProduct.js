146cc24a836be310d3fbdbee94fbf33563e78bbd
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2021-12-24 17:03:19 +0700

    done add product

--- src/components/atoms/ModalAddProduct.js
+++ src/components/atoms/ModalAddProduct.js
@@ -1,21 +1,34 @@
 import { useState } from "react";
-import { Button, FloatingLabel, Form, Modal } from "react-bootstrap";
+import { Button, FloatingLabel, Form, Image, Modal } from "react-bootstrap";
 import { NotificationManager } from "react-notifications";
 import { API } from "../../config/api";
 
 export default function ModalAddProduct({ show, handleClose }) {
+  const [preview, setPreview] = useState(null);
   const [product, setProduct] = useState({
     name: "",
     buy_price: "",
     sell_price: "",
     stock: "",
+    image: "",
   });
+
+  console.log(product);
 
   const handleChange = (e) => {
     setProduct((prevState) => ({
       ...prevState,
-      [e.target.id]: e.target.value,
+      [e.target.id]:
+        e.target.type === "file" ? e.target.files[0] : e.target.value,
     }));
+
+    if (e.target.type === "file") {
+      const fileList = e.target.files;
+
+      for (const file of fileList) {
+        setPreview(URL.createObjectURL(file));
+      }
+    }
   };
 
   const handleAddProduct = async (e) => {
@@ -24,13 +37,18 @@ export default function ModalAddProduct({ show, handleClose }) {
     try {
       const config = {
         headers: {
-          "Content-Type": "application/json",
+          "Content-Type": "multipart/form-data",
         },
       };
 
-      const body = JSON.stringify(product);
+      const formData = new FormData();
+      formData.set("image", product.image, product.image.name);
+      formData.set("name", product.name);
+      formData.set("buy_price", product.buy_price);
+      formData.set("sell_price", product.sell_price);
+      formData.set("stock", product.stock);
 
-      const response = API.post("/product", body, config);
+      const response = await API.post("/products", formData, config);
 
       if (response?.status === 200) {
         NotificationManager.success(
@@ -45,6 +63,7 @@ export default function ModalAddProduct({ show, handleClose }) {
           buy_price: "",
           sell_price: "",
           stock: "",
+          image: "",
         });
       }
     } catch (error) {
@@ -113,10 +132,25 @@ export default function ModalAddProduct({ show, handleClose }) {
               <Form.Control
                 type="number"
                 placeholder="Stock"
+                min={0}
                 onChange={handleChange}
                 value={product.stock}
               />
             </FloatingLabel>
+
+            <Form.Group controlId="image" className="mb-3 text-indigo">
+              <Form.Control type="file" onChange={handleChange} />
+            </Form.Group>
+
+            {preview && (
+              <Image
+                src={preview}
+                alt="Product Image"
+                width={100}
+                height={100}
+                thumbnail
+              />
+            )}
 
             <div className="mb-2 d-flex justify-content-end">
               <Button variant="primary" type="submit" className="px-4 me-2">
--- src/config/api.js
+++ src/config/api.js
@@ -2,10 +2,7 @@ import axios from "axios";
 
 // Create base URL API
 export const API = axios.create({
-  baseURL:
-    process.env.REACT_APP_BASE_API_URL ||
-    "https://myliterature.herokuapp.com/api/v1/" ||
-    "http://localhost:4000/api/v1",
+  baseURL: "http://localhost:4000/api/v1",
 });
 
 // Set Authorization Token Header
